bf7cf9c70b60e5363dfe7d06586d52bb6f508069
Harsh <ha.rsh4334.r@gmail.com>
2024-06-10 23:17:32 +0530

    💀

--- lib/screens/auth/login_screen.dart
+++ lib/screens/auth/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:zen/widgets/custom_elevated_button.dart';
 import 'package:zen/widgets/gap.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -17,14 +18,117 @@ class _LoginScreenState extends State<LoginScreen> {
           padding: const EdgeInsets.all(20.0),
           child: Column(
             children: [
-              const HGap(height: 40,),
+              const HGap(
+                height: 40,
+              ),
               Text(
                 'Login',
                 style: TextStyle(
                     fontSize: 36,
                     color: Theme.of(context).primaryColor,
                     fontWeight: FontWeight.w700),
-              )
+              ),
+              const HGap(
+                height: 20,
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                  label: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Icon(
+                        Icons.email,
+                        color: Theme.of(context).primaryColor,
+                      ),
+                      const Text(' Email')
+                    ],
+                  ),
+                  hintText: 'example@gmail.com',
+                  hintStyle: const TextStyle(color: Colors.grey),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                    borderSide: BorderSide(
+                      color: Theme.of(context).primaryColor,
+                      width: 2.0,
+                    ),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                    borderSide: const BorderSide(
+                      color: Colors.grey,
+                      width: 1.0,
+                    ),
+                  ),
+                ),
+                keyboardType: TextInputType.emailAddress,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter some text';
+                  }
+                  if (!RegExp(r'^[^@]+@[^@]+\.[^@]+').hasMatch(value)) {
+                    return 'Please enter a valid email address';
+                  }
+                  return null;
+                },
+              ),
+              const HGap(
+                height: 20,
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                  // labelText: 'Password',
+                  hintText: '●●●●●●●●●',
+                  hintStyle: const TextStyle(color: Colors.grey),
+                  // prefixIcon: const Icon(Icons.lock),
+                  label: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Icon(
+                        Icons.lock,
+                        color: Theme.of(context).primaryColor,
+                      ),
+                      const Text(' Password')
+                    ],
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                    borderSide: BorderSide(
+                      color: Theme.of(context).primaryColor,
+                      width: 2.0,
+                    ),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                    borderSide: const BorderSide(
+                      color: Colors.grey,
+                      width: 1.0,
+                    ),
+                  ),
+                ),
+                obscureText: true,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your password';
+                  }
+                  if (value.length < 6) {
+                    return 'Password must be at least 6 characters long';
+                  }
+                  return null;
+                },
+              ),
+              const HGap(
+                height: 20,
+              ),
+              CustomElevatedButton(
+                label: 'Login',
+                onPressd: () {},
+              ),
             ],
           ),
         ),
